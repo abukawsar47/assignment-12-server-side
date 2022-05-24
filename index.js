405dc294f8aa1c37501c5f9ea6e01324f312d9ff
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-24 19:25:48 +0600

    jwt init

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -15,8 +16,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const productCollection = client.db('look_up').collection('products');
 
+        const database = client.db('lookUp');
+
+        const productCollection = database.collection('products');
+        const orderCollection = database.collection('orders');
+        const reviewCollection = database.collection('reviews');
+        const userCollection = database.collection('users');
+
+        //get product
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
@@ -24,13 +32,62 @@ async function run() {
             res.send(products);
         })
 
+        //put users
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateEoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateEoc, options);
+            res.send(result);
+        })
+
+
+        //get product by id
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
         })
-
+        // post order
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+        // get orders
+        app.get('/order', async (req, res) => {
+            const customer = req.query.customer;
+            const query = { customer: customer };
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
+        })
+        //get reviews
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+            /*  const cursor = reviewCollection.find({});
+             const reviews = await cursor.toArray();
+             res.send(reviews); */
+        })
+        //post review
+        app.post('/review', async (req, res) => {
+            const order = req.body;
+            const result = await reviewCollection.insertOne(order);
+            res.send(result);
+        })
+        /*   app.get('/review', async (req, res) => {
+              const cursor = reviewCollection.find({});
+              const reviews = await cursor.toArray();
+              console.log(reviews);
+              res.send(reviews);
+          }) */
 
     }
     finally {
