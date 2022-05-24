b9a827267070b995eb52ef8fe7b43a48043a5a08
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-25 01:11:52 +0600

    verify admin

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ async function run() {
         const reviewCollection = database.collection('reviews');
         const userCollection = database.collection('users');
 
+        //verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
+
         //get product
         app.get('/product', async (req, res) => {
             const query = {};
@@ -50,8 +62,16 @@ async function run() {
             res.send(products);
         })
 
+        //check is admin
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
         //put admin email
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
             const updateDoc = {
@@ -59,14 +79,6 @@ async function run() {
             };
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
-        })
-
-        //check is admin
-        app.get('/admin/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = await userCollection.findOne({ email: email });
-            const isAdmin = user.role === 'admin';
-            res.send({ admin: isAdmin })
         })
 
         //put users
