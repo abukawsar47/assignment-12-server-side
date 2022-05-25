992fbf3eb0c822fdfcf32020526e25804f918de7
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-26 02:22:36 +0600

    get order id

--- index.js
+++ index.js
@@ -172,12 +172,13 @@ async function run() {
             }
         })
 
-        app.delete('/order/:id', verifyJWT, async (req, res) => {
+        app.get('/order/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.findOne(query);
             res.send(result);
         })
+
 
 
         //get reviews
